fa1f30d43d37a42363c74ad5e94ea6a47dd6ef09
faham <nafidfaham08@gmail.com>
2019-07-27 21:10:52 +0600

    update

--- clone.sh
+++ clone.sh
@@ -6,5 +6,6 @@ git clone https://github.com/TwoOreosOneCup/android_device_samsung_a5y17lte -b l
 git clone https://github.com/TwoOreosOneCup/android_device_samsung_universal7880-common -b lineage-16.0 device/samsung/universal7880-common 
 git clone https://github.com/TwoOreosOneCup/proprietary_vendor_samsung -b lineage-16.0 vendor/samsung
 git clone https://github.com/LineageOS/android_kernel_samsung_universal7880 -b lineage-16.0 kernel/samsung/universal7880
+git clone https://github.com/LineageOS/android_hardware_samsung -b lineage-16.0 hardware/samsung
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 packages/resources/devicesettings
 
